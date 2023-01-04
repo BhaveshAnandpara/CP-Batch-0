369e3466ee548f53943b5b565866a39172f55dcd
Bhavesh Anandpara <bhaveshanandpara12@gmail.com>
2023-01-04 12:14:08 +0530

    AVeryLongSum Que Solved

---
+++ WarmUp/AVeryBigSum.java
@@ -0,0 +1,40 @@
+// https://www.hackerrank.com/challenges/a-very-big-sum/problem?isFullScreen=true
+package WarmUp;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+class AVeryBigSum {
+
+    public static long aVeryBigSum(List<Long> ar) {
+
+        long result = 0;
+
+        for (int j = 0; j < ar.size(); j++) {
+            result += ar.get(j);
+        }
+
+        return result;
+
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int len = sc.nextInt();
+        List<Long> arr = new ArrayList<Long>();
+
+        for (int i = 0; i < len; i++) {
+
+            arr.add(sc.nextLong());
+
+        }
+
+        System.out.println(aVeryBigSum(arr));
+
+        sc.close();
+    }
+
+}
--- WarmUp/SimpleArraySum.java
+++ WarmUp/SimpleArraySum.java
@@ -7,15 +7,6 @@ import java.util.Scanner;
 
 class Result {
 
-    /*
-     * Complete the 'compareTriplets' function below.
-     *
-     * The function is expected to return an INTEGER_ARRAY.
-     * The function accepts following parameters:
-     * 1. INTEGER_ARRAY a
-     * 2. INTEGER_ARRAY b
-     */
-
     public static List<Integer> compareTriplets(List<Integer> a, List<Integer> b) {
 
         List<Integer> result = new ArrayList<Integer>();
