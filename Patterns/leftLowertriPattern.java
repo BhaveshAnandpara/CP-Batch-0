8e2ac2148a9216cdffc7612232057a100b845a83
Bhavesh Anandpara <bhaveshanandpara12@gmail.com>
2023-01-19 12:27:21 +0530

    leftLowertriPattern Solved

---
+++ Patterns/leftLowertriPattern.java
@@ -0,0 +1,37 @@
+package Patterns;
+import java.util.*;
+
+/*
+ 
+ *****
+ ****
+ ***
+ **
+ *
+ 
+ */
+
+public class leftLowertriPattern {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter Col = ");
+        int col = sc.nextInt();
+
+        for (int i = col; i > 0; i--) {
+
+            for (int j = 0; j < i; j++) {
+                System.out.print("*");
+            }
+            
+            System.out.println();
+
+        }
+
+        sc.close();
+
+    }
+
+}
