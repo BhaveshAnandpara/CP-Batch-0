91f9c0c4b35cf6f3c23068d84985446b7da1ebcd
Bhavesh Anandpara <bhaveshanandpara12@gmail.com>
2023-02-03 10:13:51 +0530

    sortingTheSentence que solved

--- Sorting/SortingTheSentence.java
+++ Sorting/SortingTheSentence.java
@@ -1,19 +1,19 @@
 package Sorting;
-import java.util.*;
 
 public class SortingTheSentence {
 
     public static  String sortSentence(String s) {
 
-        String[] result = new String[10];
+        String[] result = new String[10]; // size 10 bcoz maximum 10 words ( constraint given )
 
-        String[] arr = s.split(" ");
+        String[] arr = s.split(" "); // will create array of words ["is2","sentence4","This1","a3"]
         String str = "";
 
         for (int i = 0; i < arr.length; i++) {
-            result[ (int)arr[i].charAt( arr[i].length() - 1) - 48 ]  =  arr[i];
+            result[ (int)arr[i].charAt( arr[i].length() - 1) - 48 ]  =  arr[i]; // storing word based on digit so result[2] = is2
         }
 
+        // result = [ null , "This1" , "is2" , "a3" , "sentence4"  , null ....]
 
         for ( int i = 0 ; i < result.length ; i++ ) {
 
