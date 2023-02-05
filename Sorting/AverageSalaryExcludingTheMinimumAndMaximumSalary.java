2f7e93037643dc4c6d068b8c0fbb9d2ab462fdd4
Bhavesh Anandpara <bhaveshanandpara12@gmail.com>
2023-02-05 22:30:58 +0530

    AverageSalaryExcludingTheMinimumAndMaximumSalary

---
+++ Sorting/AverageSalaryExcludingTheMinimumAndMaximumSalary.java
@@ -0,0 +1,28 @@
+
+package Sorting;
+
+import java.util.*;
+
+public class AverageSalaryExcludingTheMinimumAndMaximumSalary {
+
+    public static double average(int[] salary) {
+
+        Arrays.sort( salary );
+        int sum = 0;
+
+        for( int i = 1 ; i < salary.length - 1 ; i++){
+            sum = sum + salary[i];
+        }
+
+        return sum / (double)(salary.length - 2);
+    }
+
+    public static void main(String[] args) {
+
+        int[] salary = {4000,3000,1000,2000};
+        
+        System.out.println( average( salary ) );
+
+    }
+    
+}
