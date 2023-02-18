d1b993ada99d8e8542dd570c0f0f5245dc37d16b
Bhavesh Anandpara <bhaveshanandpara12@gmail.com>
2023-02-18 09:27:00 +0530

    Added Comments on RomanToInt

--- Strings/RomanToInteger.java
+++ Strings/RomanToInteger.java
@@ -15,13 +15,17 @@ public class RomanToInteger {
     map['D' - 'A' ] = 500;
     map['M' - 'A' ] = 1000;
         
-    char prevChar = s.charAt(0);
+
+    // IV --> 5 - 1 , VI --> 5 + 1
+    char prevChar = s.charAt(0); //storing prev Char
     int result = 0;
     
 
     for (int i = 1; i < s.length(); i++) {
         
-        if(  map[s.charAt(i) - 'A'] > map[prevChar - 'A' ]  ) {
+        // chcking if prev Char is smaller than current means ( IV ) case occurs so minus and add it to result
+        // else just add it to the result
+        if(  map[s.charAt(i) - 'A'] > map[prevChar - 'A' ]  ) { 
             result += ( map[ s.charAt(i) - 'A' ] -map[ prevChar - 'A' ] ) ;
 
             if( i + 1 < s.length() ){
@@ -34,7 +38,7 @@ public class RomanToInteger {
 
         else{
 
-            result += map[ prevChar - 'A' ];
+            result += map[ prevChar - 'A' ]; 
             prevChar = s.charAt(i);
 
         }
