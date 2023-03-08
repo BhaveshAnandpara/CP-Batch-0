47bbfbdf54e2acf521e866ad34e36981ac5b0cca
Bhavesh Anandpara <bhaveshanandpara12@gmail.com>
2023-03-08 21:47:27 +0530

    Generate Paranthesis Updated

--- Recursion/Questions/GenerateParentheses.java
+++ Recursion/Questions/GenerateParentheses.java
@@ -11,16 +11,18 @@ public class GenerateParentheses {
     // <cmg "ImageNotes\GenerateParantesis.jpg">
     public static List<String> generateParenthesis(int n) {
 
-        generateResult("", n);
+        // generateResult("", n);
+        generateResult("", 0 , 0 , n);
         return result;
 
     }
 
-
+    /*
+     * // Correct Approach but could have been better
     public static void generateResult(String p, int n) {
 
         int countOfCloseB = 0;
-        int pLen = p.length();
+        int pLen = p.length( );
 
         if (n == 0) {
             result.add(p);
@@ -48,6 +50,28 @@ public class GenerateParentheses {
         }
 
     }
+     */
+
+
+     //Easy Approach
+     public static void generateResult(String p , int close , int open  , int n ){
+
+        if( p.length() == n*2 ) {
+            result.add(p);
+            return;
+        }
+
+        // Add '(' if there is still space for brackets
+        if( open < n  ){
+            generateResult( p+'(' , close, open + 1, n);
+        }
+
+        //Only add ')' if '(' count is more
+        if( close < open ){
+            generateResult( p+')' , close + 1, open, n);
+        }
+
+     }
 
     public static void main(String[] args) {
    
